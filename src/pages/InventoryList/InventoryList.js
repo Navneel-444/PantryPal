ba6e97d0f2d2e9138b983d23ef75ade9af73b579
Navneel Nandran <navneel.nandran@outlook.com>
2024-04-15 00:55:49 -0400

    add basic styling

--- src/pages/InventoryList/InventoryList.js
+++ src/pages/InventoryList/InventoryList.js
@@ -1,15 +1,15 @@
 import "./InventoryList.scss";
 import InventoryItem from '../../components/ListItem/ListItem';
 import PantryTab from "../../components/PantryTabs/PantryTabs";
+import ManualAdd from "../../components/ManualAdd/ManualAdd";
 
 export default function InventoryListPage() {
+    const inventory = `invertory`;
     return (
         <main className="inventory">
             <PantryTab />
             <h1 className="inventory__title">Inventory List</h1>
-            <ul className="inventory__list">
-                <InventoryItem />
-            </ul>
+            <ManualAdd pantry={inventory} />
         </main>
     )
 }
