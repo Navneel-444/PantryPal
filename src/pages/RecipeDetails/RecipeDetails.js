131306216d3143a7236f9c72e6abd7e67367e485
Navneel Nandran <navneel.nandran@outlook.com>
2024-04-15 20:29:41 -0400

    changes

--- src/pages/RecipeDetails/RecipeDetails.js
+++ src/pages/RecipeDetails/RecipeDetails.js
@@ -6,7 +6,9 @@ import { useParams } from "react-router-dom"
 export default function RecipeDetailsPage() {
     const apiKey = `?apiKey=c12d33dfd1594f018031bd984180f312`
     const { recipeId } = useParams();
-    const [details, setDetails] = useState()
+    const [details, setDetails] = useState(null)
+    const [inv, setInv] = useState(null)
+
 
     useEffect(() => {
         const getRecipeDetails = async () => {
@@ -20,9 +22,47 @@ export default function RecipeDetailsPage() {
         getRecipeDetails()
     }, [])
 
-    if (!details) {
-        return <p>Loading</p>;
-    }
+    // useEffect(() => {
+    //     const getGroceryList = async () => {
+    //         try {
+    //             const res = await axios.get(`http://localhost:8080/inventory`)
+    //             setInv(res.data);
+    //         } catch (e) {
+    //             console.error(e)
+    //         }
+    //     }
+    //     getGroceryList()
+    // }, [])
+
+
+    // function findCommonItems(array1, array2) {
+    //     if (!Array.isArray(array1) || !Array.isArray(array2)) return [];
+    //     const commonItems = [];
+    //     array1.forEach(item1 => {
+    //         console.log(item1);
+    //         array2.forEach(item2 => {
+    //             console.log(item2);
+    //             if (item1.item === item2.name) {
+    //                 commonItems.push(item2);
+    //             }
+    //         });
+    //     });
+
+    //     return commonItems;
+    // }
+
+    // useEffect(() => {
+    //     const commonItems = findCommonItems(inv, details);
+    //     console.log(commonItems);
+    // }, [inv, details]);
+
+    // console.log(inv);
+    // console.log(details);
+
+
+    // if (!details) {
+    //     return <p>Loading</p>;
+    // }
 
     return (
         <>
